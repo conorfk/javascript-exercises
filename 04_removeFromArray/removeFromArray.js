d7c326b316c4a750d30113ab2e27342ca386c69b
conorfk <concfk8080@gmail.com>
2024-07-02 10:40:11 -0400

    Finished Arrays and Loops Segment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    // This works for non-repeating bad values in array
+    // args.forEach((arg) => {
+    //     const i = array.indexOf(arg);
+    //     if (i > -1) {
+    //         array.splice(i, 1);
+    //     }
+    // });
+    // return array;
+
+    args.forEach((arg) => {
+        for (let i = 0; i < array.length; i++){
+            if (arg === array[i]) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (low, high) {
+    let sum = 0;
+    if (typeof low != 'number' || typeof high != 'number') return "ERROR";
+    if (low < 0 || high < 0) return "ERROR";
+    
+    if (low > high) {
+        let temp = low;
+        low = high;
+        high = temp;
+    }
+    for (low; low <= high; low++){
+        sum += low;
+    }
+    return sum;
 };
 
 // Do not edit below this line
